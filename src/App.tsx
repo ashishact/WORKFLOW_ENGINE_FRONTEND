941e83116826b7dbc558cabea6dbc3f33c61f31e
Ashish Charan Tandi <act.jnv@gmail.com>
2021-05-27 02:41:19 +0530

    ADD: Added monaco editor

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Editor from "@monaco-editor/react";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Editor
+                    theme="vs-dark"
+                    height="90vh"
+                    defaultLanguage="yaml"
+                    defaultValue="# some comment"
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
